275c2a2f8a1a48c01302dfc598f38fc2302caa68
Timofey Tagaev <timofey.tagaev.2004.12@gmail.com>
2023-09-23 14:32:30 +0700

    First prototype of a service

---
+++ ADT/main.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+struct Group {
+	char* name;
+	int members;
+};
+
+struct User {
+	struct BirthDate {};
+	char* name;
+
+};
+
+struct SocialMedia {
+
+};
+
